022177e0736240f5c416d4ac0867c906de50893d
sskt1969 <41769896+sskt1969@users.noreply.github.com>
2018-07-27 16:34:00 +0530

    Update and rename sri23.py to la.py

---
+++ la.py
@@ -0,0 +1,5 @@
+num = int(input())
+if (num % 2) == 0:
+   print("{} is Even".format(num))
+else:
+   print("{} is Odd".format(num))
--- sri23.py
+++
@@ -1,7 +0,0 @@
-r = int(input())
-if (r > 0):
-        print("Positive numbers")
-elif r == 0:
-        print("Zero")
-else:
-        print("Negative numbers")
